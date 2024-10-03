e11211fd5bc034ede43d3586a47b926327b56dfd
ccchdx <2919129325@qq.com>
2024-10-03 11:37:39 +0800

    some package

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-1 调试时使用`npm link`将此包挂载到全局
+1 初始化文件 `npm init`
+2 在根目录下创建`bin/index.js`文件作为入口文件并添加如下代码
+```javascript
+"bin": {
+  "create-cli": "/bin/index.js",
+}
+```
+3 调试时使用`npm link`将此包链接到全局
+
+
+相关依赖
+实现一个脚手架，通常会用到以下依赖包：
+- commander: 命令行处理工具
+- chalk: 命令行输出美化工具, 例如改变字体样式等
+- inquirer: 命令行交互工具
+- ora: 终端loading美化工具
+- git-clone: 下载项目模板工具
+- figlet：终端生成艺术字
+- fs-extra: 用来操作本地目录
--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,68 @@
 #!/usr/bin/env node
 // 告诉浏览器用node来执行这个文件
 // process.argv 返回当前进程的所有命令行参数，返回值是一个数组，前2个元素是node命令路径和被执行的文件路径
-console.log(process.argv)
+// console.log(process.argv)
+// create-hdx --help    --help是自带的
+const { program } = require('commander')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+const ora = require('ora')
+const figlet = require('figlet')
+
+// ----- figlet -----
+figlet('hello World!', function(err, data) {
+  if(err) {
+    console.log(err)
+    return
+  }
+  console.log(data)
+})
+
+// ----- ora -----
+// const spinner = ora('wait for loading...').start()
+
+// ----- inquirer -----
+// inquirer.prompt([
+//   {
+//     type: 'input',
+//     name: 'name',
+//     message: 'Your project name:',
+//     validate: (input) => {
+//       if (!input) {
+//         return 'Project name is required.'
+//       }
+//       return true
+//     }
+//   },
+//   {
+//     type: 'input',
+//     name: 'description',
+//     message: 'Project description:'
+//   },
+//   {
+//     type: 'confirm',
+//     name: 'ts',
+//     message: 'install ts?',
+//     default: false
+//   }
+// ]).then((answers) => {
+//   console.log(answers)
+// }).catch((err) => {
+//   console.error(chalk.red(err.message))
+// })
+
+// ----- chalk -----
+// console.log(chalk.red('yuiioii'));
+
+
+// ----- commander ------
+// program.name('hdx').usage('<command> [option]')
+// program
+//   .option('-d, --debug', 'output extra debugging')
+//   .option('-s, --small', 'samll pizza size')
+//   .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+// program.parse(process.argv)
+
+// const options = program.opts();
+// console.log(options);
